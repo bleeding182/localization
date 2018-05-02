ee389fa98c99e55e29747ce710f9920d76729015
David Medenjak <davidmedenjak@gmail.com>
2018-05-02 20:48:47 +0200

    Fixed tab/spaces

--- writer/ios/ios_writer.go
+++ writer/ios/ios_writer.go
@@ -38,22 +38,22 @@ const iosStringsDictTemplate = `
 {{end -}}
 <plist version="1.0">
 {{- range $p := $.Plurals}}
-	<dict>
-		<key>{{.Key}}</key>
+    <dict>
+        <key>{{.Key}}</key>
         <dict>
-			<key>NSStringLocalizedFormatKey</key>
-			<string>%#@{{.Key}}Key@</string>
-			<key>{{.Key}}Key</key>
-			<dict>
-				<key>NSStringFormatSpecTypeKey</key>
-				<string>NSStringPluralRuleType</string>
-				<key>NSStringFormatValueTypeKey</key>
-				<string>d</string>
-			{{- range $q, $v := .Values}}
+            <key>NSStringLocalizedFormatKey</key>
+            <string>%#@{{.Key}}Key@</string>
+            <key>{{.Key}}Key</key>
+            <dict>
+                <key>NSStringFormatSpecTypeKey</key>
+                <string>NSStringPluralRuleType</string>
+                <key>NSStringFormatValueTypeKey</key>
+                <string>d</string>
+            {{- range $q, $v := .Values}}
                 <key>{{$q}}</key>
                 <string>{{$v.Value}}</string>
-			{{- end}}
-			</dict>
+            {{- end}}
+            </dict>
         </dict>
     </dict>
 {{end}}
@@ -69,28 +69,20 @@ import Foundation
 // swiftlint:disable line_length
 public struct Strings {
 
-	{{- range $g := $.Groups}}
-	public struct {{.Name | camelcase}} {
-		{{- range $as := $g.Strings}}
-		static let {{.Key | camelcase}} = Strings.localized("{{.Key}}", value: "{{.Value}}"
-			{{- if .Comment -}}
-				, comment: "{{.Comment}}"
-			{{- end -}}
-		)
-		{{- end}}
-	}
-	{{end}}
+    {{- range $g := $.Groups}}
+    public struct {{.Name | camelcase}} {
+        {{- range $as := $g.Strings}}
+        static let {{.Key | camelcase}} = Strings.localized("{{.Key}}", value: "{{.Value}}"
+            {{- if .Comment -}}
+                , comment: "{{.Comment}}"
+            {{- end -}}
+        )
+        {{- end}}
+    }
+    {{end}}
 
-	// settings this closure allows you to use a custom localization provider, such as OneSky over-the-air
-    // by default, NSLocalizedString will load the strings from the main bundle's Localizable.strings file
-    public static var customLocalizationClosure: ((String, String?, Bundle, String, String) -> String)? = nil
-    
     public static func localized(_ key: String, tableName: String? = nil, bundle: Bundle = Bundle.main, value: String, comment: String = "") -> String {
-        if let closure = Strings.customLocalizationClosure {
-            return closure(key, tableName, bundle, value, comment)
-        } else {
-            return NSLocalizedString(key, tableName: tableName, bundle: bundle, value: value, comment: comment)
-        }
+        return NSLocalizedString(key, tableName: tableName, bundle: bundle, value: value, comment: comment)
     }
 }
 `
